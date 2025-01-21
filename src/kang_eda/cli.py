2ed1b4c9945ba4e3d4d79937ed2de52e5785d59a
stundrg <khr0106@naver.com>
2025-01-21 10:51:24 +0900

    cli.py 수정

--- src/kang_eda/cli.py
+++ src/kang_eda/cli.py
@@ -2,13 +2,15 @@ from president_speech.db.parquet_interpreter import get_parquet_full_path
 import pandas as pd
 import typer
 
-def group_by_count(keyword: str):
+def group_by_count(keyword: str, asc: bool, row: int):
+    # TODO : ascending, 출력 rows size
+    # pytest 코드를 만들어  볼까요
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
     fdf = df[df['speech_text'].str.contains(keyword, case = False)]
     gdf = fdf.groupby("president").size().reset_index(name = "count")
-    sdf = gdf.sort_values(by='count', ascending = False).reset_index(drop = True)
-    print(sdf.to_string(index = False))
+    sdf = gdf.sort_values(by='count', ascending = asc).reset_index(drop = True)
+    print(sdf.head(row).to_string(index = False))
 
 def entry_point():
     typer.run(group_by_count)
