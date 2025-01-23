9592632c430dfa6e931da4c4957970ed514c2bdd
stundrg <khr0106@naver.com>
2025-01-23 17:27:53 +0900

    나는 커밋하지만 지금 내가 뭘 하고 있는지 모르겠다면 이게 믿어지는가...

--- tests/first_test.py
+++ tests/first_test.py
@@ -87,6 +87,56 @@ def test_wc():
     assert df.iloc[1]['count'] == 438
     assert df.iloc[11]['count'] == 1
     
-    for p_name, s_count in presidents_speeches.items():
-        president_row = df[df['president'] ==p_name]
-        assert president_row.iloc[0]['count']==s_count
+#    for p_name, s_count in presidents_speeches.items():
+#       president_row = df[df['president'] ==p_name]
+#       assert president_row.iloc[0]['count']==s_count
+
+    for i, (p_name, s_count) in enumerate(presidents_speeches.items()):
+        assert df.iat[i,1] == s_count
+
+
+def test_all_count_keywordsum():
+    # given
+    # global dict
+
+    # when
+    df = group_by_count(keyword = "자유", asc = False, keyword_sum = True)
+
+    # then
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 12
+    assert 'keyword_sum' in df.columns
+    #assert (df['keyword_sum'] >= df['count'])
+    # count 보다 keyword_sum 이 크거나 같음을 확인 assert
+
+    # 1 all
+    assert all(df['keyword_sum'] >= df['count'])
+    # 2 
+    for row in df.itertuples():
+        assert row.keyword_sum >= row.count
+
+    # 3 - 열의 순서를 알고 있고 위치기반으로 다루고 싶을 때
+    for i in range(len(df)):
+        keyword_sum = df.iloc[i,df.columns.get_loc("keyword_sum")]
+        count = df.iloc[i,df.columns.get_loc("count")]
+        assert keyword_sum >=count
+    # 4 
+    for i in range(len(df)):
+        keyword_sum = df.loc[i,"keyword_sum"]
+        count = df.loc[i, 'count']
+        assert keyword_sum>= count
+
+
+    # 5 
+    for i in range (len(df)):
+        keyword_sum = df.iat[i,2]
+        count = df.iat[i, 1]
+        assert keyword_sum >=count
+
+
+
+
+
+
+    #for i, (p_name, s_count) in enumerate(presidents_speeches.items()):
+     #   assert df.iat[i,1] == s_count
